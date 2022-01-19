76dcb4e25057d2186d1716ff5b24aec623fc66b9
Leonardo Molina <leomolina0501@hotmail.com>
2022-01-18 21:37:44 -0300

    Criando formulário de contato e integrando lista com formulário

---
+++ lib/screens/contact_form.dart
@@ -0,0 +1,67 @@
+import 'package:bytebank/models/contato.dart';
+import 'package:flutter/material.dart';
+
+class ContactForm extends StatefulWidget {
+  @override
+  State<ContactForm> createState() => _ContactFormState();
+}
+
+class _ContactFormState extends State<ContactForm> {
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _numeroContaController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Novo contato'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _nameController,
+              decoration: InputDecoration(
+                labelText: 'Nome',
+              ),
+              style: TextStyle(
+                fontSize: 24.0,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: TextField(
+                controller: _numeroContaController,
+                decoration: InputDecoration(
+                  labelText: 'Número da conta',
+                ),
+                style: TextStyle(
+                  fontSize: 24.0,
+                ),
+                keyboardType: TextInputType.number,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 16.0),
+              child: SizedBox(
+                width: double.maxFinite,
+                child: RaisedButton(
+                  child: Text('Gravar'),
+                  onPressed: () {
+                    final String name = _nameController.text;
+                    final int? numConta =
+                        int.tryParse(_numeroContaController.text);
+
+                    final contato = new Contato(name, numConta);
+                    Navigator.pop(context, contato);
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
